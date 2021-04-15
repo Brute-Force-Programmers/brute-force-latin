2dcadc66fe0419106a5e5a594fd1928e3b2f37e2
Grayson Stricker <graysonstricker@Graysons-MacBook-Pro.local>
2021-04-15 17:30:21 -0400

    add new component

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function HomePage() {
+    return (
+        <div>
+            <h1>Homepage</h1>
+            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi dolores ipsa architecto? Hic, omnis? Eum reiciendis porro aliquid neque quis!</p>
+        </div>
+    )
+}
+
+export default HomePage
