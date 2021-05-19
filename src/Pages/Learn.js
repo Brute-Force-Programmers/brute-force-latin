38e2f9cab79ae62117b6a01fb4b0f45f5c3379b7
Grayson Stricker <g-strick@Graysons-MacBook-Air.local>
2021-05-18 23:04:37 -0400

    add in input area for quiz feature

--- src/Pages/Learn.js
+++ src/Pages/Learn.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+import UserQuizInput from '../components/UserQuizInput'
 const Learn = () => {
     return (
         <div>
             <NavBar/>
             <h1>Learn Latin</h1>
+            {/* this is where I need to add in functionality */}
+            <UserQuizInput/>
+
         </div>
     )
 }
