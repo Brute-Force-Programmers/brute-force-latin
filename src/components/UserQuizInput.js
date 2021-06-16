c6ac794c29dc5cfa79e21105fdcfff9beb33fd41
G Strick <graysonstricker@gmail.com>
2021-06-16 15:04:13 -0400

    update user quiz input

--- src/components/UserQuizInput.js
+++ src/components/UserQuizInput.js
@@ -2,13 +2,16 @@ import React from "react";
 import { InputGroup, InputGroupAddon, InputGroupText, Input } from "reactstrap";
 
 const UserQuizInput = (props) => {
+  console.log(props);
   return (
     <div>
+      <h1>{props.type}</h1>
+      <h3>{props.declension}</h3>
       <InputGroup>
         <InputGroupAddon addonType="prepend">
-          <InputGroupText>Gate (feminine)</InputGroupText>
+          <InputGroupText>{props.word}</InputGroupText>
         </InputGroupAddon>
-        <Input placeholder="type here" />
+        <Input placeholder={props.answer} />
       </InputGroup>
     </div>
   );
