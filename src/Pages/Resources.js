6ffce98700a5ee9d665d19a1880481a7c58a9ba6
Rob Pupo <pupo.robert@gmail.com>
2022-01-27 17:23:38 -0500

    addition of lingua latina to resources

--- src/Pages/Resources.js
+++ src/Pages/Resources.js
@@ -7,6 +7,7 @@ const Resources = () => {
             <NavBar/>
             <h1>Resources</h1>
             {/* <a href = 'https://wcdrutgers.net/Latin.htm'>Latin by the Dowling Method</a> */}
+            <a href = 'https://lingualatina.dk/wp/'>Next Steps - Lingua Latina</a>
         </div>
     )
 }
