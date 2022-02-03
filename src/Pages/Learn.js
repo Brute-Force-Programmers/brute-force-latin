224f2fc4f8efac8449d17913b69f0bd3ac932eb3
Christian Madajski <cmad7317@tutanota.com>
2022-02-03 11:40:31 -0500

    change how JSON data is used in Learn.js

--- src/Pages/Learn.js
+++ src/Pages/Learn.js
@@ -2,26 +2,32 @@ import React from "react";
 import NavBar from "../components/NavBar";
 import UserQuizInput from "../components/UserQuizInput";
 
-
+// read JSON info into object form
 const readJSON = require('../vocab/AdjectivesTest.json')
-
-
-
+// get all the 1st level key values from the object
+const mainKeys = Object.keys(readJSON);
+// iterator i determines what word is being read
+let i = 1;
+// integer indices will not work, so keys are used to access data
+let currType = readJSON[mainKeys[i]].category;
+let currSection = readJSON[mainKeys[i]].sectionTitle;
+let currWord = readJSON[mainKeys[i]].word;
+let currGender = readJSON[mainKeys[i]].gender;
+let currSingPlur = readJSON[mainKeys[i]].pluralOrSingular;
+let currAnswer = readJSON[mainKeys[i]].answer.join(' ');
 
 const Learn = (props) => {
-  
-
   return (
     
     <div>
       <NavBar />
       <UserQuizInput
-        type={readJSON.largeMS.category}
-        sectionTitle={readJSON.largeMS.sectionTitle}
-        word={readJSON.largeMS.word}
-        gender={readJSON.largeMS.gender}
-        singular={readJSON.largeMS.pluralOrSingular}
-        answer={(readJSON.largeMS.answer).join(' ')}
+        type={currType}
+        sectionTitle={currSection}
+        word={currWord}
+        gender={currGender}
+        singular={currSingPlur}
+        answer={currAnswer}
       />
     </div>
   );
