b014f506c5ced25be6cff4955b6b8f9708929ba2
Rob Pupo <pupo.robert@gmail.com>
2022-02-06 13:31:51 -0500

    include Submit Button to userQuizInput

--- src/components/UserQuizInput.js
+++ src/components/UserQuizInput.js
@@ -1,29 +1,27 @@
 import React from "react";
-import { InputGroup, InputGroupAddon, InputGroupText, Input } from "reactstrap";
-import {Button, Form, FormGroup, Label,  FormText, Col, Row, Card, CardBody, Table, CardHeader,SubmitButton,
-} from 'reactstrap';
-// import SubmitButton from "./SubmitButton";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  Input,
+  SubmitButton,
+} from "reactstrap";
+
 
 const UserQuizInput = (props) => {
   return (
     <div>
-      <FormGroup row>
-      <Col sm={1}></Col>
-         <Col sm={10}>
-          <h1>{props.type}</h1>
-          <h3>{props.sectionTitle}</h3>
-          <InputGroup >
-          {/* <InputGroup className="w-75" style = {{marginLeft: "10px"}} > */}
-            <InputGroupAddon addonType="prepend">
-              <InputGroupText>{props.word} {" (" + props.gender + " "}
-              {"" +props.singular + ")"}</InputGroupText>
-            </InputGroupAddon>
-            <Input placeholder={props.answer} />
-          </InputGroup>
-          <SubmitButton style = {{marginTop: "10px"}}></SubmitButton>
-
-        </Col>
-        </FormGroup>
+      <h1> {props.type} </h1>
+      <h3> {props.sectionTitle} </h3>
+      <InputGroup>
+        <InputGroupAddon addonType="prepend">
+          <InputGroupText>
+            {props.word} {" (" + props.gender + " "} {"" + props.singular + ")"}
+          </InputGroupText>
+        </InputGroupAddon>
+        <Input placeholder={props.answer} />
+      </InputGroup>
+      <SubmitButton style={{ marginTop: "10px", }}></SubmitButton>
     </div>
   );
 };
