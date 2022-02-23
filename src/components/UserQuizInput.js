8f560b878337b01d95549539fd6c1170f672e6a3
Rob Pupo <pupo.robert@gmail.com>
2022-02-22 19:30:59 -0500

    Add explanatory comments and remove excess logic from quiz component

--- src/components/UserQuizInput.js
+++ src/components/UserQuizInput.js
@@ -1,70 +1,52 @@
-import React, {useEffect, useState} from "react";
-import {
-  InputGroup,
-  InputGroupAddon,
-  InputGroupText,
-  Input,
-} from "reactstrap";
-
-// this is a test
+import React, { useEffect, useState } from "react";
+import { InputGroup, InputGroupAddon, InputGroupText, Input } from "reactstrap";
 
 const UserQuizInput = (props) => {
-  const [inputValues, setInputValue] = useState(null)
-  const [isAnswered, setIsAnswered] = useState("Submit")
-  const [count, setCount] = useState(0)
+  // Including states for the quiz
+  const [inputValues, setInputValue] = useState(0);
+  const [isAnswered, setIsAnswered] = useState("Submit");
+  const [count, setCount] = useState(0);
   const onChange = (event) => {
-    setInputValue(event.target.value)
-  }
-  useEffect(() => {
-    console.log(inputValues)
-  }, [inputValues]); 
-  
-  const handleSubmit = (event, inputValues,isAnswered,count) => {
-    if (isAnswered == 'Submit') {
-      setIsAnswered('Next Word')
+    setInputValue(event.target.value);
+  };
+
+  // Function to capture values from user input and print to console
+  const handleSubmit = (event, inputValues, isAnswered, count) => {
+    if (isAnswered == "Submit") {
+      setIsAnswered("Next Word");
+    } else {
+      setIsAnswered("Submit");
+      setInputValue(null);
+      setCount(count + 1);
     }
-    else {
-      setIsAnswered('Submit')
-      setInputValue(null)
-      // document.getElementById("TheInput").reset();
-      // event.target.reset()
-      // getelementby
-      // setInput({ Input1: "", Input2: "" });
-      setCount(count + 1)
-    }
-    console.log('Submission')
-    console.log(inputValues, isAnswered, count)
+    console.log(inputValues, isAnswered, count);
     event.preventDefault();
-  }
+  };
   return (
     <div>
       <h1> {props.type} </h1>
       <h3> {props.sectionTitle} </h3>
-      <InputGroup onChange={(event) => onChange(event)}
-       >
+      {/* Included onChange event to update the answerValue state when user types into the input */}
+      <InputGroup onChange={(event) => onChange(event)}>
         <InputGroupAddon addonType="prepend">
           <InputGroupText>
             {props.word} {" (" + props.gender + " "} {"" + props.singular + ")"}
           </InputGroupText>
         </InputGroupAddon>
-        <Input 
-          // value = {inputValues} 
-          placeholder={props.answer} 
-          // id='TheInput' 
-          // name = 'TheInput'
-          value = {inputValues}
-          type = 'text'
-        />
-        <Input 
-          type = 'submit' 
-          onClick={(event) => handleSubmit(event, inputValues,isAnswered,count)} 
-          value = {isAnswered}
-
+        <Input placeholder={props.answer} value={inputValues} type="text" />
+        {/* Included onclick event to console log answer submission */}
+        <Input
+          type="submit"
+          onClick={(event) => handleSubmit(event, inputValues, isAnswered, count)
+          }
+          value={isAnswered}
         />
       </InputGroup>
 
-      <p>The states are: {inputValues}, {isAnswered}, {count}</p>
-
+      {/* Showing quiz states for simplicity */}
+      <p>InputValue: {inputValues}</p>
+      <p>IsAnsered:{isAnswered}</p>
+      <p>Count:{count}</p>
     </div>
   );
 };
