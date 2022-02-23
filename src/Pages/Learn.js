e10866f84a6f6459699f33c7386a53a4b8f82d1c
Rob Pupo <pupo.robert@gmail.com>
2022-02-23 18:35:16 -0500

    Remove unnescessary code from LearnJS

--- src/Pages/Learn.js
+++ src/Pages/Learn.js
@@ -2,36 +2,15 @@ import React from "react";
 import NavBar from "../components/NavBar";
 import UserQuizInput from "../components/UserQuizInput";
 
-
 // read JSON info into object form
 const readJSON = require('../vocab/AdjectivesTest.json')
-// get all the 1st level key values from the object
-const mainKeys = Object.keys(readJSON);
-// iterator i determines what word is being read
-let i = 1;
-// integer indices will not work, so keys are used to access data
-let currType = readJSON[mainKeys[i]].category;
-let currSection = readJSON[mainKeys[i]].sectionTitle;
-let currWord = readJSON[mainKeys[i]].word;
-let currGender = readJSON[mainKeys[i]].gender;
-let currSingPlur = readJSON[mainKeys[i]].pluralOrSingular;
-let currAnswer = readJSON[mainKeys[i]].answer.join(' ');
 
 const Learn = (props) => {
-  // console.log(UserQuizInput.json)
   return (
     
     <div>
       <NavBar />
-      <UserQuizInput
-        type={currType}
-        sectionTitle={currSection}
-        word={currWord}
-        gender={currGender}
-        singular={currSingPlur}
-        answer={currAnswer}
-        json = {readJSON}
-        />
+      <UserQuizInput json = {readJSON}/>
     </div>
 
   );
