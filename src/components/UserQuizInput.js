bf808c64795917c074676853311330648d4663a6
Rob Pupo <pupo.robert@gmail.com>
2022-02-27 14:38:53 -0500

    Update next word logic

--- src/components/UserQuizInput.js
+++ src/components/UserQuizInput.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { InputGroup, InputGroupAddon, InputGroupText, Input } from "reactstrap";
+import React, { useState } from "react";
+import { InputGroup, InputGroupAddon, InputGroupText, Input , FormFeedback, FormGroup, Form} from "reactstrap";
 
 // function to compare user input vs expected answer
 const UserQuizInput = (props) => {
@@ -9,8 +9,8 @@ const UserQuizInput = (props) => {
 
   // Including states for the quiz
   const [inputValues, setInputValue] = useState(null);
-  const [isAnswered, setIsAnswered] = useState("Submit");
-  const [count, setCount] = useState(1);
+  // const [isAnswered, setIsAnswered] = useState("Submit");
+  const [count, setCount] = useState(0);
   const [rightCount, setRight] = useState(0);
   const [wrongCount, setWrong] = useState(0);
   const [section, setSection] = useState(theJSON[mainKeys[count]].category);
@@ -23,7 +23,8 @@ const UserQuizInput = (props) => {
   const [answer, setAnswer] = useState(
     theJSON[mainKeys[count]].answer.join(" ")
   );
-  console.log(section, type, word, gender, singularPlural, answer);
+  const [inputValid, setInputValid] = useState(null)
+  // console.log(section, type, word, gender, singularPlural, answer);
 
   // update the inputValue when user types in Input box
   const onChange = (event) => {
@@ -31,12 +32,14 @@ const UserQuizInput = (props) => {
   };
   // Function to capture values from user input and print to console
   const handleSubmit = (event, inputValues, isAnswered, count) => {
-    if (isAnswered === "Submit") {
-      answerComparison(answer, inputValues);
-      setIsAnswered("Next Word");
+    if (answerComparison(answer, inputValues)) {
+      
+      // setIsAnswered("Next Word");
+      updateStates()
     } else {
-      setIsAnswered("Submit");
-      updateStates();
+      // setIsAnswered("Submit");
+      // updateStates();
+      alert("Wrong!")
     }
     console.log(section, type, word, gender, singularPlural, answer);
     event.preventDefault();
@@ -46,20 +49,25 @@ const UserQuizInput = (props) => {
   const answerComparison = (expectedAnswer, userSubmission) => {
     if (expectedAnswer === userSubmission) {
       setRight(rightCount + 1);
+      return true
     } else {
-      setWrong(wrongCount + 1);
+      // setWrong(wrongCount + 1);
+      return false
+
     }
   };
   // consolidated JSON-related state updates into a single function
   const updateStates = () => {
     setInputValue(null);
-    setCount(count + 1);
+    // setCount(count + 1);
     setSection(theJSON[mainKeys[count]].category);
     setType(theJSON[mainKeys[count]].sectionTitle);
     setWord(theJSON[mainKeys[count]].word);
     setGender(theJSON[mainKeys[count]].gender);
     setSingularPlural(theJSON[mainKeys[count]].pluralOrSingular);
     setAnswer(theJSON[mainKeys[count]].answer.join(" "));
+    setInputValid(false)
+    console.log(inputValid)
   };
 
   return (
@@ -67,29 +75,39 @@ const UserQuizInput = (props) => {
       <h1> {type} </h1>
       <h3> {section} </h3>
       {/* Included onChange event to update the answerValue state when user types into the input */}
+      <Form>
+
+      <FormGroup>
+
+
       <InputGroup onChange={(event) => onChange(event)}>
         <InputGroupAddon addonType="prepend">
           <InputGroupText>
             {word} {" (" + gender + " "} {"" + singularPlural + ")"}
           </InputGroupText>
         </InputGroupAddon>
-        <Input placeholder={answer} value={inputValues} type="text" />
+        <Input placeholder={answer} value={inputValues} type="text" 
+        invalid = {inputValid}
+        />
         {/* Included onclick event to capture answer submission */}
         <Input
           type="submit"
           onClick={(event) =>
-            handleSubmit(event, inputValues, isAnswered, count)
+            handleSubmit(event, inputValues, count)
           }
-          value={isAnswered}
         />
+        <FormFeedback invalid>This bollocks is not showing!!!!</FormFeedback>
       </InputGroup>
+      </FormGroup>
+      </Form>
 
       {/* Showing quiz states for simplicity */}
       <p>InputValue: {inputValues}</p>
-      <p>IsAnsered:{isAnswered}</p>
       <p>Count:{count}</p>
       <p>Right:{rightCount}</p>
       <p>Wrong:{wrongCount}</p>
+
+
     </div>
   );
 };
